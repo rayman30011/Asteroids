4e72dfa69a0c822623a35e9594f96ca12946ed00
Mikhail Pavlov <rayman30011@gmail.com>
2021-06-17 18:41:07 +0600

    добавил лазер

--- Source/Asteroids/Private/Weapons/LaserWeapon.cpp
+++ Source/Asteroids/Private/Weapons/LaserWeapon.cpp
@@ -3,3 +3,40 @@
 
 #include "Weapons/LaserWeapon.h"
 
+#include "DrawDebugHelpers.h"
+#include "Enitites/Spaceship.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void ULaserWeapon::StartFire(AActor* Instigator)
+{
+	if (CanTrace && GetWorld())
+	{
+		CanTrace = false;
+		TraceLaser(Instigator);
+		FTimerHandle Timer;
+		GetWorld()->GetTimerManager().SetTimer(Timer, this, &ULaserWeapon::ReloadCompleted, Rate, false);
+	}
+}
+
+void ULaserWeapon::TraceLaser(AActor* Instigator) const
+{
+	TArray<FHitResult> Hits;
+	const auto Spaceship = Cast<ASpaceship>(Instigator);
+	const auto StartLocation = Spaceship ? Spaceship->GetMuzzleWorldLocation() : Instigator->GetActorLocation();
+	const auto EndLocation = StartLocation + Instigator->GetActorForwardVector() * 1000;
+	TArray<AActor*> Ignore;
+	if (UKismetSystemLibrary::SphereTraceMulti(GetWorld(), StartLocation, EndLocation, 15.f, TraceTypeQuery1, true, Ignore, EDrawDebugTrace::ForDuration, Hits, true))
+	{
+		for (auto Hit: Hits)
+		{
+			const auto Actor = Hit.Actor.Get();
+			if (!Actor) return;
+			Actor->TakeDamage(2.f, FDamageEvent{}, Spaceship ? Spaceship->GetController() : nullptr, Instigator);
+		}
+	}
+}
+
+void ULaserWeapon::ReloadCompleted()
+{
+	CanTrace = true;
+}
--- Source/Asteroids/Public/Weapons/LaserWeapon.h
+++ Source/Asteroids/Public/Weapons/LaserWeapon.h
@@ -13,5 +13,17 @@ UCLASS(BlueprintType, Blueprintable)
 class ASTEROIDS_API ULaserWeapon : public UBaseWeapon
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float Rate = 1.2f;
+
+public:
+	virtual void StartFire(AActor* Instigator) override;
+
+private:
+	bool CanTrace = true;
 	
+	void TraceLaser(AActor* Instigator) const;
+	void ReloadCompleted();
 };

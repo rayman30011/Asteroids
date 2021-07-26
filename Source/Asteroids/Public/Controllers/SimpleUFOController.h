ff9ae3f1f64abb860c707413c6d4ded5c7df1d90
Mikhail Pavlov <rayman30011@gmail.com>
2021-07-26 13:43:02 +0600

    добавил контроллер

---
+++ Source/Asteroids/Public/Controllers/SimpleUFOController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "SimpleUFOController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTEROIDS_API ASimpleUFOController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
